48da82eea0cd9522a3cfbe0deb1d1ecab8661825
rfunix <rafinha.unix@gmail.com>
2018-08-07 23:15:51 -0300

    :lipstick:

--- lib/item_handler.ex
+++ lib/item_handler.ex
@@ -2,21 +2,21 @@ defmodule Lix.Item.Handler do
   use GenServer
 
   @name :handler_item
-  @handler_info %{handler_item: [queue: "test_item", callback: "process_item"]}
+  @handler_info %{handler_item: [queue: "queue/test_item", callback: "process_item"]}
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: @name)
+  end
+
+  def handling() do
+    Lix.Handler.run(@name)
+    handling()
   end
 
   @impl true
   def init(args) do
     Lix.Handler.register(@handler_info)
     {:ok, args}
-  end
-
-  def handling() do
-    Lix.Handler.run(@name)
-    handling()
   end
 
   @impl true
