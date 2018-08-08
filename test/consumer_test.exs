3b0208f25728a3d4c8eacf8c416e43306385f307
rfunix <rafinha.unix@gmail.com>
2018-08-07 23:16:49 -0300

    Add tests to delete_message

--- test/consumer_test.exs
+++ test/consumer_test.exs
@@ -11,8 +11,19 @@ defmodule ConsumerTest do
       {ExAws, [], [request!: fn _message -> @sqs_message end]}
     ] do
       assert ["message test"] == Lix.Consumer.get_message("queue")
-      assert called(ExAws.SQS.receive_message("queue/queue"))
+      assert called(ExAws.SQS.receive_message("queue"))
       assert called(ExAws.request!(@sqs_message))
+    end
+  end
+
+  test "delete_message" do
+    with_mocks([
+      {ExAws.SQS, [], [delete_message: fn _queue, _receipt_handle -> {:ok} end]},
+      {ExAws, [], [request!: fn _message -> {:ok} end]}
+    ]) do
+      Lix.Consumer.delete_message("queue", "receipt_handle")
+      assert called(ExAws.SQS.delete_message("queue", "receipt_handle"))
+      assert called(ExAws.request!({:ok}))
     end
   end
 end
