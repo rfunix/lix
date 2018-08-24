4e7bf54099e4cbded2065996adc6a3a7cb04a764
rfunix <rafinha.unix@gmail.com>
2018-08-23 23:16:09 -0300

    Add ExAws.SNS

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Lix.MixProject do
       #
       {:ex_aws, "~> 2.0"},
       {:ex_aws_sqs, "~> 2.0"},
+      {:ex_aws_sns, "~> 2.0"},
       {:poison, "~> 3.0"},
       {:hackney, "~> 1.9"},
       {:sweet_xml, "~> 0.6"},
