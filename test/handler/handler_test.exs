6e4af1bb57513b259c198412ca5d166cdcfaf130
rfunix <rafinha.unix@gmail.com>
2018-08-27 21:29:42 -0300

    Add handler publish message test

--- test/handler/handler_test.exs
+++ test/handler/handler_test.exs
@@ -6,8 +6,16 @@ defmodule HandlerTest do
   defp handler_name_fixture, do: :handler_test
 
   defp handler_fixture,
-    do: %{handler_test: [queue: "queue/test_handler", callback: "process_test"]}
+    do: %{
+      handler_test: [
+        queue: "queue/test_handler",
+        callback: "process_test",
+        topic_arn: topic_arn_fixture()
+      ]
+    }
 
+  defp message_to_send_fixture, do: "message_test"
+  defp topic_arn_fixture, do: "topic_arn_test"
   defp sqs_message_fixture, do: ["message test"]
   defp receipt_handler_fixture, do: %{receipt_handle: "test_receipt_handle"}
 
@@ -31,6 +39,32 @@ defmodule HandlerTest do
       Lix.Handler.confirm_processed_callback(handler_name_fixture(), receipt_handler_fixture())
       Process.sleep(10)
       assert called(ExAws.SQS.delete_message("queue/test_handler", "test_receipt_handle"))
+      assert called(ExAws.request!({:ok}))
+    end
+  end
+
+  test "confirm processed callback with publish_message" do
+    with_mocks([
+      {ExAws.SQS, [], [delete_message: fn _queue, _receipt_handle -> {:ok} end]},
+      {ExAws.SNS, [], [publish: fn _message, [topic_arn: _topic_arn] -> {:ok} end]},
+      {ExAws, [], [request!: fn _message -> {:ok} end]}
+    ]) do
+      Lix.Handler.Manager.register(handler_fixture())
+
+      Lix.Handler.confirm_processed_callback(
+        handler_name_fixture(),
+        receipt_handler_fixture(),
+        message_to_send_fixture()
+      )
+
+      Process.sleep(10)
+      assert called(ExAws.SQS.delete_message("queue/test_handler", "test_receipt_handle"))
+
+      assert called(
+               ExAws.SNS.publish(message_to_send_fixture(), topic_arn: topic_arn_fixture())
+             )
+
+      assert called(ExAws.request!({:ok}))
       assert called(ExAws.request!({:ok}))
     end
   end
