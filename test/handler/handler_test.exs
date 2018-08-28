bfbdbf995f1c7a4c49565e7d38e4f64d3af2c07c
rfunix <rafinha.unix@gmail.com>
2018-08-27 21:16:23 -0300

    Refact fixtures

--- test/handler/handler_test.exs
+++ test/handler/handler_test.exs
@@ -3,18 +3,21 @@ defmodule HandlerTest do
 
   import Mock
 
-  @handler_name :handler_test
-  @handler %{handler_test: [queue: "queue/test_handler", callback: "process_test"]}
-  @sqs_message ["message test"]
-  @receipt_handle %{receipt_handle: "test_receipt_handle"}
+  defp handler_name_fixture, do: :handler_test
+
+  defp handler_fixture,
+    do: %{handler_test: [queue: "queue/test_handler", callback: "process_test"]}
+
+  defp sqs_message_fixture, do: ["message test"]
+  defp receipt_handler_fixture, do: %{receipt_handle: "test_receipt_handle"}
 
   # TODO: Improve this test
   test "handler run" do
     with_mocks([
-      {Lix.Consumer, [], [get_message: fn _queue -> @sqs_message end]}
+      {Lix.Consumer, [], [get_message: fn _queue -> sqs_message_fixture() end]}
     ]) do
-      Lix.Handler.Manager.register(@handler)
-      Lix.Handler.run(@handler_name)
+      Lix.Handler.Manager.register(handler_fixture())
+      Lix.Handler.run(handler_name_fixture())
       assert called(Lix.Consumer.get_message("queue/test_handler"))
     end
   end
@@ -24,8 +27,8 @@ defmodule HandlerTest do
       {ExAws.SQS, [], [delete_message: fn _queue, _receipt_handle -> {:ok} end]},
       {ExAws, [], [request!: fn _message -> {:ok} end]}
     ]) do
-      Lix.Handler.Manager.register(@handler)
-      Lix.Handler.confirm_processed_callback(@handler_name, @receipt_handle)
+      Lix.Handler.Manager.register(handler_fixture())
+      Lix.Handler.confirm_processed_callback(handler_name_fixture(), receipt_handler_fixture())
       Process.sleep(10)
       assert called(ExAws.SQS.delete_message("queue/test_handler", "test_receipt_handle"))
       assert called(ExAws.request!({:ok}))
